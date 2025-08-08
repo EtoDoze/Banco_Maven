e5a7eb1b53690865b604cb5d4b108895f270adaa
Nailan-Nobre <anaclarasantoa13@gmail.com>
2025-08-08 08:50:15 -0300

    a

--- banco_maven/src/main/java/br/edu/ifpi/Model/PessoaFisica.java
+++ banco_maven/src/main/java/br/edu/ifpi/Model/PessoaFisica.java
@@ -1,14 +1,31 @@
 package br.edu.ifpi.Model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pessoa_fisica")
 
 public class PessoaFisica extends Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "cpf", nullable = false, unique = true)
     private String cpf;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-    public  String getCpf() {
+    public String getCpf() {
         return this.cpf;
     }
 
